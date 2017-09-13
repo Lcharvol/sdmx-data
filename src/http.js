9c254f94e1b13d647408eb77462e71dbb0463e65
lcharvol <lcharvol@student.42.fr>
2017-09-13 17:51:33 +0200

    add mongo request

--- src/http.js
+++ src/http.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import http from 'http';
 import debug from 'debug';
+import { map, reduce, toPairs, flatten } from 'ramda';
 import compression from 'compression';
 import morgan from 'morgan-debug';
 import initFiles from './files';
-import { initMongo } from './mongo';
 
 const logger = debug('sdmx:hhtp');
 
@@ -15,6 +15,77 @@ const error = (err, req, res, next) => {
   res.status(500).json({
     message: err.toString(),
   });
+};
+
+const getMongoData = (db, coll) => (req, res, next) => {
+  const collection = db.collection(coll);
+  collection.find({}).toArray((err, docs) => {
+    if (err) return next(err);
+    logger('Found types in mongodb');
+    res.json(docs);
+  });
+};
+
+const getSearchParams = lang => searchValue => ({ $or:
+  [
+    { [`name.${lang}`]: new RegExp(searchValue, 'i') },
+    { [`description.${lang}`]: new RegExp(searchValue, 'i') },
+  ],
+});
+
+const getRestQuery = lang => ([props, searchValue]) => ({ $or:
+  [
+    { [`${props}.${lang}`]: new RegExp(searchValue, 'i') },
+    { [`${props}`]: new RegExp(searchValue, 'i') },
+  ],
+});
+
+const getFilteredDataflows = db => (req, res, next) => {
+  const collection = db.collection('dataflows');
+  const { search, f, ...rest } = req.query;
+  const { lang } = req.params;
+  let searchParams = [];
+  let fieldsValue;
+  const restQuery = flatten(map(getRestQuery(lang), toPairs(rest)));
+  if (search) {
+    const searchValues = search.indexOf(',') >= 0 ? search.split(',') : [search];
+    searchParams = map(getSearchParams(lang), searchValues);
+  }
+  if (f) {
+    fieldsValue = f.indexOf(',') >= 0 ? f.split(',') : [f];
+  }
+  const query = { $and: [...searchParams, ...restQuery] };
+  collection.find(query, fieldsValue).toArray((err, docs) => {
+    if (err) return next(err);
+    logger('Found dataflows in mongodb');
+    res.json(docs);
+  });
+};
+
+const loadTypes = db => db.collection('types').find().toArray();
+
+const loadDataflows = db => db.collection('dataflows').aggregate([{ $group: { _id: '$type', count: { $sum: 1 } } }]).toArray();
+
+const getMongoGroupTypes = db => (req, res) => {
+  const promises = [loadTypes(db), loadDataflows(db)];
+  Promise.all(promises)
+    .then(([types, dataflows]) => {
+      const reducedTypes = reduce(
+        (acc, { _id, avatar }) => (
+          { ...acc, [_id]: avatar }), {}, types,
+      );
+      res.json(map(dataflow => ({ avatar: reducedTypes[dataflow._id], count: dataflow.count }), dataflows));
+    });
+};
+
+const initMongo = db => {
+  const app = express();
+  app
+    .get('/types', getMongoData(db, 'types'))
+    .get('/dataflows', getMongoData(db, 'dataflows'))
+    .get('/groupTypes', getMongoGroupTypes(db))
+    .get('/dataflows/:lang/', getFilteredDataflows(db));
+  return app;
 };
 
 const init = ctx => {
--- src/mongo.js
+++ src/mongo.js
@@ -1,37 +1,7 @@
 import mongodb from 'mongodb';
 import debug from 'debug';
-import express from 'express';
 
 const logger = debug('sdmx:mongo');
-
-const getMongoData = (db, coll) => (req, res) => {
-  const collection = db.collection(coll);
-  collection.find({}).toArray((err, docs) => {
-    if (err) throw err;
-    logger('Found types in mongodb');
-    res.json(docs);
-  });
-};
-
-const getFilteredDataflows = db => (req, res) => {
-  const collection = db.collection('dataflows');
-  const { search } = req.query;
-  const { lang } = req.params;
-  collection.find({ name: { lang: search } }).toArray((err, docs) => {
-    if (err) throw err;
-    logger('Found dataflows in mongodb');
-    res.json(docs);
-  });
-};
-
-export const initMongo = db => {
-  const app = express();
-  app
-    .get('/types', getMongoData(db, 'types'))
-    .get('/dataflows/:lang/', getFilteredDataflows(db))
-    .get('/dataflows', getMongoData(db, 'dataflows'));
-  return app;
-};
 
 const init = ctx => {
   const { config: { mongo } } = ctx;
